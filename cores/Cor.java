0290ace692dcb3c9976270017fb3e8c37159129d
pombozap <72285470+pombozap@users.noreply.github.com>
2023-05-04 11:37:52 -0300

    Movendo Cor.java para pacote cores.

--- cores/Cor.java
+++ cores/Cor.java
@@ -1,3 +1,5 @@
+package cores;
+
 // Cor é um tipo enumerado que enumera as quatro cores de Ludo.
 // (Tomaz) Nota: Acho que especialmente essa "classe" podia estar fora do pacote e ser pública. Afinal, ela vai ser usada sempre para saber o número correspondente a cada cor.
 public enum Cor {
