d16b175245c52e19bf01ec32d6605516eb36f854
mcgema <65995291+mcgema@users.noreply.github.com>
2023-05-04 10:57:00 -0300

    v1

---
+++ Cor.java
@@ -0,0 +1,5 @@
+// Cor é um tipo enumerado que enumera as quatro cores de Ludo.
+// (Tomaz) Nota: Acho que especialmente essa "classe" podia estar fora do pacote e ser pública. Afinal, ela vai ser usada sempre para saber o número correspondente a cada cor.
+public enum Cor {
+    verde, vermelho, amarelo, azul
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+import regras.*;
+
+// A Main serve para testar.
+// (Tomaz) Usar Model em vez de Tester para testar DEPOIS DE MODEL ESTAR COMPLETA!!!
+public class Main {
+    public static void main (String[] args) {
+        Tabuleiro jogo = new Tabuleiro();
+        Tester teste = new Tester();
+    }
+}
