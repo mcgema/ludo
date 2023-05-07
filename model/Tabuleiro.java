050d3af80e91722b7bc9a2f361a85d2fc404bb90
mcgema <mclarawerneck@gmail.com>
2023-05-07 13:52:35 -0300

    tentativa

--- model/Tabuleiro.java
+++ model/Tabuleiro.java
@@ -243,11 +243,27 @@ class Tabuleiro {
     // (Tomaz) Nota: essa função não está imprimindo conforme o enunciado! Ela DEVERIA imprimir as cores dos jogadores em ordem de colocação (aka: de pontos). FALTANDO!
 
     public int compareTo(int[] a, int[] b) {
-        return Double.compare(a[0], b[0]);
+        return Integer.compare(a[0], b[0]);
     }
     
     protected void termina () {
-        System.out.printf("nao");
+        System.out.printf("nao"); int distanciasTotais[][] = new int[4][1];
+        for (Piao[] aux: arrayPioes) {
+            for (Piao p: aux) {
+                distanciasTotais[p.getCorNum()][0] += p.getPosicao();
+            }
+        }
+        Arrays.sort(distanciasTotais);
+        for (int i=0; i<4; i++){
+           System.out.printf("Jogador %d: %d pontos\n",distanciasTotais[i],distanciasTotais[i][0]);
+
+        }
+        for (int i=0; i<4; i++){
+            for (Cor c: Cor.values()) {
+                if (c.ordinal() == i) System.out.printf("Jogador %s: %d pontos\n",c.toString(),distanciasTotais[c.ordinal()]);
+            }
+        }
+
     }
     
     
