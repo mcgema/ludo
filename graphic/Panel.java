b427f30e303021fc25e6443aec51638b1e20ede8
mcgema <mclarawerneck@gmail.com>
2023-05-17 16:50:50 -0300

    test

--- graphic/Panel.java
+++ graphic/Panel.java
@@ -2,7 +2,7 @@ package graphic;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
-
+import model.*;
 
 public class Panel extends JPanel {
     public void paintComponent(Graphics g){
@@ -10,28 +10,29 @@ public class Panel extends JPanel {
         Graphics2D g2d=(Graphics2D) g;
         //g.drawString("Maria Clara Werneck", 500,250);
         // Desenha retângulo
-        double leftX=300.0;
-        double topY=50.0;
-        double larg=600.0;
-        double alt=600.0;
-        Rectangle2D rt=new Rectangle2D.Double(leftX,topY,larg,alt);
+        Rectangle2D rt=new Rectangle2D.Double(300,50,600,600);
         g2d.setPaint(Color.BLACK);
         g2d.draw(rt);
 
         //QUADRADOS MENORES
         Rectangle2D rt1=new Rectangle2D.Double(300,50,240,240);
-        g2d.setPaint(Color.BLUE);
+        g2d.setPaint(Color.RED);
         g2d.fill(rt1);
         Rectangle2D rt2=new Rectangle2D.Double(300,410,240,240);
-        g2d.setPaint(Color.RED);
+        g2d.setPaint(Color.BLUE);
         g2d.fill(rt2);
         Rectangle2D rt3=new Rectangle2D.Double(660,50,240,240);
-        g2d.setPaint(Color.YELLOW);
+        g2d.setPaint(Color.GREEN);
         g2d.fill(rt3);
         Rectangle2D rt4=new Rectangle2D.Double(660,410,240,240);
-        g2d.setPaint(Color.GREEN);
+        g2d.setPaint(Color.YELLOW);
         g2d.fill(rt4);
-
+        int x = 660;
+        for (int i=0; i<6; i++){
+            Rectangle2D r=new Rectangle2D.Double(x,50+40*i,40,40);
+            g2d.setPaint(Color.BLACK);
+            g2d.draw(r);
+        }
         //g2d.fill(rt);
     }
 }
