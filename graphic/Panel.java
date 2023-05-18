d779bddbca28fff6dcfea1e83c212deb877351a2
mcgema <mclarawerneck@gmail.com>
2023-05-18 14:06:56 -0300

    tentativas

--- graphic/Panel.java
+++ graphic/Panel.java
@@ -2,9 +2,56 @@ package graphic;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.*;
-import model.*;
+import java.time.Year;
+
+import cores.*;
 
 public class Panel extends JPanel {
+
+    public void parteTabuleiro(Graphics g, int cor, int x, int y){
+        Graphics2D g2d=(Graphics2D) g;
+        Rectangle2D rt1=new Rectangle2D.Double(x, y, 240, 240);
+        // MELHORAR
+
+        if (cor == 0){
+            g2d.setPaint(Color.RED);
+
+        }
+        else{ 
+            g2d.setPaint(Color.GREEN);}
+        g2d.fill(rt1);
+        y += 240;
+        for (int j=0; j<3; j++){
+            for (int i=0; i<6; i++){
+                rt1 = new Rectangle2D.Double(x+40*i, y+40*j, 40, 40 );
+                g2d.setPaint(Color.BLACK);
+                g2d.draw(rt1); 
+            }
+        }
+
+        for (int j=0; j<5; j++){
+            rt1 = new Rectangle2D.Double(x+40+40*j, y+40, 40, 40 );
+            // MELHORAR COM DICT
+            if (cor == 0){
+                g2d.setPaint(Color.RED);
+    
+            }
+            else{ 
+                g2d.setPaint(Color.GREEN);}
+            g2d.fill(rt1); 
+        }
+
+
+        rt1 = new Rectangle2D.Double(x+40, y+40, 40, 40 );
+        if (cor == 0){
+            g2d.setPaint(Color.RED);
+
+        }
+        else{ 
+            g2d.setPaint(Color.GREEN);}
+        g2d.fill(rt1); 
+
+    }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2d=(Graphics2D) g;
@@ -15,24 +62,36 @@ public class Panel extends JPanel {
         g2d.draw(rt);
 
         //QUADRADOS MENORES
-        Rectangle2D rt1=new Rectangle2D.Double(300,50,240,240);
-        g2d.setPaint(Color.RED);
-        g2d.fill(rt1);
-        Rectangle2D rt2=new Rectangle2D.Double(300,410,240,240);
-        g2d.setPaint(Color.BLUE);
-        g2d.fill(rt2);
-        Rectangle2D rt3=new Rectangle2D.Double(660,50,240,240);
-        g2d.setPaint(Color.GREEN);
-        g2d.fill(rt3);
-        Rectangle2D rt4=new Rectangle2D.Double(660,410,240,240);
-        g2d.setPaint(Color.YELLOW);
-        g2d.fill(rt4);
-        int x = 660;
-        for (int i=0; i<6; i++){
-            Rectangle2D r=new Rectangle2D.Double(x,50+40*i,40,40);
-            g2d.setPaint(Color.BLACK);
-            g2d.draw(r);
+        for (int i=0; i<4; i++){
+            Rectangle2D rt1=new Rectangle2D.Double(300,50,240,240);
+            g2d.setPaint(Color.RED);
+            g2d.fill(rt1);
+
+
         }
+        parteTabuleiro(g, 0, 300,50);
+
+        g2d.rotate(-1.57079632679, 300+240, 410);
+
+        parteTabuleiro(g, 1, 300,410-240);
+       // g2d.rotate(-1.57079632679);
+
+
+        // Rectangle2D rt2=new Rectangle2D.Double(300,410,240,240);
+        // g2d.setPaint(Color.BLUE);
+        // g2d.fill(rt2);
+        // Rectangle2D rt3=new Rectangle2D.Double(660,50,240,240);
+        // g2d.setPaint(Color.GREEN);
+        // g2d.fill(rt3);
+        // Rectangle2D rt4=new Rectangle2D.Double(660,410,240,240);
+        // g2d.setPaint(Color.YELLOW);
+        // g2d.fill(rt4);
+        // int x = 660;
+        // for (int i=0; i<6; i++){
+        //     Rectangle2D r=new Rectangle2D.Double(x,50+40*i,40,40);
+        //     g2d.setPaint(Color.BLACK);
+        //     g2d.draw(r);
+        // }
         //g2d.fill(rt);
     }
 }
