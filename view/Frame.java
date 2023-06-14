ced26146983e44a062e6ed2a18c97333a39f92dd
pombozap <tomazpereiraneves@gmail.com>
2023-06-14 14:50:25 -0300

    remocao de repaint no nre game pela frame

--- view/Frame.java
+++ view/Frame.java
@@ -31,7 +31,6 @@ public class Frame extends JFrame {
         novoJogoButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 controller.novoJogo();
-                view.repaint();
             }
         });
 
