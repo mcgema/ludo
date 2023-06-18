09b5b403a9898e75e5acbedcf595bc3af0e292a6
mcgema <mclarawerneck@gmail.com>
2023-06-18 00:30:02 -0300

    gui

--- cores/Cor.java
+++
@@ -1,6 +0,0 @@
-package cores;
-
-// Cor é um tipo enumerado que enumera as quatro cores de Ludo.
-public enum Cor {
-    vermelho, verde, amarelo, azul
-}
---
+++ gui/cores/Cor.java
@@ -0,0 +1,6 @@
+package cores;
+
+// Cor é um tipo enumerado que enumera as quatro cores de Ludo.
+public enum Cor {
+    vermelho, verde, amarelo, azul
+}
